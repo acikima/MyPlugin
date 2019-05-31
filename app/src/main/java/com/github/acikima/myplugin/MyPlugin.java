179b12c0a4374d2ab6cb3733a41ee6e896410e9e
Ima <fatimatul@sage42.net>
2019-05-31 09:49:32 +0800

    Initial

---
+++ app/src/main/java/com/github/acikima/myplugin/MyPlugin.java
@@ -0,0 +1,13 @@
+package com.github.acikima.myplugin;
+
+public class MyPlugin {
+    String greet;
+
+    public MyPlugin() {
+        this.greet = "Hey";
+    }
+
+    public String getGreet() {
+        return greet;
+    }
+}
